3c7f8ad800766ca9cdf4a6331db03452b39ad039
Antelmira <elmira.abdullina.1999@mail.ru>
2022-10-30 22:22:46 +0300

    Создание класса для алгоритма сортировки пузырьком

--- src/SortBubble.java
+++ src/SortBubble.java
@@ -1,2 +1,22 @@
 public class SortBubble {
+    public static void main(String[] params) {
+        int[] array = new int[] {64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36};
+        bubbleSort(array);
+
+    }
+
+    public static void bubbleSort(int[] array) {
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i - 1] > array[i]) {
+                    int tmp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = tmp;
+                    isSorted = false;
+                }
+            }
+        }
+    }
 }
